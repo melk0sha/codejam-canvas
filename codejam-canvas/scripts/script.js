87b8f60d7ec30ead5e94197a96f8adcc4e92441a
melk0sha <melk0shadev@gmail.com>
2019-10-27 16:16:29 +0300

    feat: realize all main functions

--- codejam-canvas/scripts/script.js
+++ codejam-canvas/scripts/script.js
@@ -1,21 +1,51 @@
-let frameMatrix4x4 = [];
-let frameMatrix32x32 = [];
+document.addEventListener('DOMContentLoaded', event => {
+    let canvasContext = getCanvasContext();
+    let radioButtons = document.getElementsByTagName('input');
+    radioButtons[0].onclick = () => fetch("assets/data/4x4.json")
+        .then(response => response.json().then(json => drawMatrix4x4(json, canvasContext)));
+    radioButtons[1].onclick = () => fetch("assets/data/32x32.json")
+        .then(response => response.json().then(json => drawMatrix32x32(json, canvasContext)));
+    radioButtons[2].onclick = () => drawCanvasImage(canvasContext);
+});
 
-fetch("assets/data/4x4.json")
-    .then(response => frameMatrix4x4 = response.json());
+function getCanvasContext() {
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.width = 512;
+    canvas.height = 512;
+    return ctx;
+}
 
-fetch("assets/data/32x32.json")
-    .then(response => frameMatrix32x32 = response.json());
+function drawMatrix32x32(frame, ctx) {
+    frame.forEach((row, i) => {
+        row.forEach((column, j) => {
+            ctx.fillStyle = 'rgba(' + column[0] + ', ' + column[1] + ', ' + column[2] + ')';
+            ctx.fillRect(j*16, i*16, (j+1)*16, (i+1)*16);
+        });
+    });
+}
 
+function drawMatrix4x4(frame, ctx) {
+    frame.forEach((row, i) => {
+        row.forEach((column, j) => {
+            ctx.fillStyle = '#' + column;
+            ctx.fillRect(j*128, i*128, (j+1)*128, (i+1)*128);
+        });
+    });
+}
 
-// window.onload = function() {
-//     var ex1 = document.getElementsByTagName('input');
-//     for (let ex of ex1) {
-//         ex.onclick = this.handler;
-//     }
-// }
+function drawCanvasImage(ctx) {
+    let imgObj = new Image();
 
-// function handler() {
-//     alert('clicked');
-// }
+    imgObj.onload = function() {
+        let w = canvas.width;
+        let nw = imgObj.naturalWidth;
+        let nh = imgObj.naturalHeight;
+        let aspect = nw / nh;
+        let h = w / aspect;
+        canvas.height = h;
+        ctx.drawImage(imgObj, 0, 0, w, h);
+    };
 
+    imgObj.src = 'assets/data/image.png';
+}
